3a820c4da6cd2d940acb2165d985ecd0caf7772f
fhermes <fhermes@asesconsulting.com>
2018-08-07 18:08:44 -0300

    no funciono inyectar los headers a mano. voy a tener que hacerlo bien

--- src/main/java/farguito/sarlanga/seed/websocket/CombateWebsocketController.java
+++ src/main/java/farguito/sarlanga/seed/websocket/CombateWebsocketController.java
@@ -2,17 +2,23 @@ package farguito.sarlanga.seed.websocket;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.util.HtmlUtils;
 
 import farguito.sarlanga.seed.acciones.Accion;
@@ -28,10 +34,20 @@ import farguito.sarlanga.seed.niveles.RepositorioDeNiveles;
 import farguito.sarlanga.seed.websocket.objetos.TurnoAccionIN;
 import farguito.sarlanga.seed.websocket.objetos.TurnoAccionOUT;
 
-@RestController
-@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-public class CombateWebsocketController implements CombateController {
+@Controller
+@Scope(scopeName = "websocket", proxyMode = ScopedProxyMode.TARGET_CLASS) //genero uno por conexion
+public class CombateWebsocketController implements CombateController, ApplicationListener<SessionConnectEvent> {
+
+	@Autowired
+    private SimpMessageSendingOperations template;
 	
+	//TODO: hacerlo bien. y lindo. no esta chanchada.
+	String header = "{simpMessageType=MESSAGE, contentType=application/json;charset=UTF-8}";
+	String header_2 = "{simpMessageType=MESSAGE, conversionHint=method 'turnoAccion' parameter -1, contentType=application/json;charset=UTF-8, simpSessionId=afizlwxo}";
+	
+
+	private String id;
+
 	@Autowired
 	private FabricaDeCriaturas fabCriaturas;
 	
@@ -62,22 +78,42 @@ public class CombateWebsocketController implements CombateController {
 		combate.iniciar(personajes, niveles.get(1).getEnemigos());	
 	}
 	
-	
+///*
     @MessageMapping("/accion") //lo que viene de JS
-    @SendTo("/combate/mensajes") //a donde lo mando
-    public TurnoAccionOUT turnoAccion(TurnoAccionIN turnoAccion) throws Exception { 
-    	String mensaje = combate.accionar(turnoAccion.getObjetivoId(), combate.getPersonajeActivo().getAcciones().get(turnoAccion.getAccionId()));
-    	
-        return new TurnoAccionOUT(HtmlUtils.htmlEscape(mensaje));
+    //@SendToUser("/combate/mensajes") //a donde lo mando
+    public void turnoAccion(TurnoAccionIN turnoAccion) throws Exception { 
+    	combate.accionar(turnoAccion.getObjetivoId(), combate.getPersonajeActivo().getAcciones().get(turnoAccion.getAccionId()));    	
     }
-    
-	public void loggear(String mensaje) {    	
-		turnoEnemigo(mensaje);
+//*/
+	
+/*
+    @MessageMapping("/accion") //lo que viene de JS
+    @SendToUser("/combate/mensajes") //a donde lo mando
+    public TurnoAccionOUT turnoAccion(TurnoAccionIN turnoAccion) throws Exception { 
+    	String mensaje = "@SendToUser ";
+        mensaje += combate.accionar(turnoAccion.getObjetivoId(), combate.getPersonajeActivo().getAcciones().get(turnoAccion.getAccionId()));
+    	return new TurnoAccionOUT(HtmlUtils.htmlEscape(mensaje));
+    }
+//*/
+	public void loggear(String mensaje) { 		
+		String canal = "/combate/mensajes";
+		Map<String, Object> headers = new HashMap<>();
+		headers.put("simpSessionId", id);
+		
+		if(id != null) {
+			mensaje = id+": "+mensaje;
+			template.convertAndSendToUser(id, canal, new TurnoAccionOUT(HtmlUtils.htmlEscape(mensaje)), headers);
+			template.convertAndSend(canal, new TurnoAccionOUT(HtmlUtils.htmlEscape(mensaje)), headers);
+		}
+	}
+
+
+	@Override
+	public void onApplicationEvent(SessionConnectEvent event) {
+		MessageHeaders headers = event.getMessage().getHeaders();
+		this.id = SimpMessageHeaderAccessor.getSessionId(headers);		
 	}
 	
-	@SubscribeMapping("/combate/mensajes")
-	public TurnoAccionOUT turnoEnemigo(String mensaje) {
-        return new TurnoAccionOUT(HtmlUtils.htmlEscape(mensaje));
-	}
+	
 
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,55 +1,66 @@
 var stompClient = null;
 
 function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
+	$("#connect").prop("disabled", connected);
+	$("#disconnect").prop("disabled", !connected);
+	if (connected) {
+		$("#conversation").show();
+	} else {
+		$("#conversation").hide();
+	}
+	$("#greetings").html("");
 }
 
 function connect() {
-    var socket = new SockJS('/mensajes');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/combate/mensajes', function (turnoAccionOut) {
-            showMensaje(JSON.parse(turnoAccionOut.body).mensaje);
-        });
-    });
+	var socket = new SockJS('/mensajes');
+	stompClient = Stomp.over(socket);
+	stompClient.connect({}, function(frame) {
+		setConnected(true);
+		console.log('Connected: ' + frame);
+
+		stompClient.subscribe('/user/combate/mensajes', 
+			function(turnoAccionOut) {
+				showMensaje(JSON.parse(turnoAccionOut.body).mensaje);
+			});
+/*
+		stompClient.subscribe('/combate/mensajes', 
+			function(turnoAccionOut) {
+				showMensaje(JSON.parse(turnoAccionOut.body).mensaje);
+			});
+*/		
+	});
 }
 
 function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
+	if (stompClient !== null) {
+		stompClient.disconnect();
+	}
+	setConnected(false);
+	console.log("Disconnected");
 }
 
-function sendName() {
-    stompClient.send("/app/accion", {}, 
-    		JSON.stringify(
-    				{'accionId': $("#accionId").val()
-    			    ,'objetivoId': $("#objetivoId").val()
-    			    } ));
+function sendAccion() {
+	stompClient.send("/app/accion", {}, JSON.stringify({
+		'accionId' : $("#accionId").val(),
+		'objetivoId' : $("#objetivoId").val()
+	}));
 }
 
 function showMensaje(message) {
-	console.log("frula")
-    $("#mensajes").append("<tr><td>" + message + "</td></tr>");
+	$("#mensajes").append("<tr><td>" + message + "</td></tr>");
 }
 
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
-    $( "#send" ).click(function() { sendName(); });
+$(function() {
+	$("form").on('submit', function(e) {
+		e.preventDefault();
+	});
+	$("#connect").click(function() {
+		connect();
+	});
+	$("#disconnect").click(function() {
+		disconnect();
+	});
+	$("#send").click(function() {
+		sendAccion();
+	});
 });
